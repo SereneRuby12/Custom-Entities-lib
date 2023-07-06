232690815faf4f0e858fe61b6cfc061f3e03a537
estebanfer <estebanfer2004@gmail.com>
2023-07-06 11:09:07 -0300

    Update version to 1.1

--- custom_entities.lua
+++ custom_entities.lua
@@ -1,6 +1,6 @@
 meta = {
     name = "Custom-Entities-Library",
-    version = "1.0b",
+    version = "1.1",
     author = "Estebanfer",
     description = "A library for creating custom entities"
 }
