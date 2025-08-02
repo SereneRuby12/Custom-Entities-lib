ba24837f7426e75ea38e7e550cf80d51a398d2ca
sereneruby12 <sereneruby12@gmail.com>
2025-08-02 01:13:06 -0300

    Update version

--- custom_entities.lua
+++ custom_entities.lua
@@ -1,6 +1,6 @@
 meta = {
     name = "Custom-Entities-Library",
-    version = "1.1c",
+    version = "1.1d",
     author = "SereneRuby12",
     description = "A library for creating custom entities"
 }
